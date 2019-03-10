f44598c297643ed7d68e05353147ba44268ce35b
Dheeraj Kotwani <dheeraj.kotwani41@gmail.com>
2019-03-10 22:05:08 +0530

    Add Activities

--- app/src/main/java/project/tronku/miwokapplication/MainActivity.java
+++ app/src/main/java/project/tronku/miwokapplication/MainActivity.java
@@ -8,19 +8,47 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView numbers=findViewById(R.id.numbers);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TextView numbers=findViewById(R.id.numbers);
+        TextView family=findViewById(R.id.family);
+        TextView colours=findViewById(R.id.colors);
+        TextView phrases=findViewById(R.id.phrases);
+
         numbers.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i=new Intent(MainActivity.this,Numbers.class);
                 startActivity(i);
+            }
+        });
 
+        family.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(MainActivity.this,Family.class);
+                startActivity(i);
+            }
+        });
+
+        colours.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(MainActivity.this,Colours.class);
+                startActivity(i);
+            }
+        });
+
+        phrases.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(MainActivity.this,Phrases.class);
+                startActivity(i);
             }
         });
 
