c4c8e7f347021e4554aacfcb8d7b527df74b384f
Dheeraj Kotwani <dheeraj.kotwani41@gmail.com>
2019-03-18 01:14:13 +0530

    Update Number Activity

--- app/src/main/java/project/tronku/miwokapplication/Numbers.java
+++ app/src/main/java/project/tronku/miwokapplication/Numbers.java
@@ -25,16 +25,16 @@ public class Numbers extends AppCompatActivity {
 
         ArrayList<Word> words = new ArrayList<Word>();
 
-        words.add(new Word("One","ek"));
-        words.add(new Word("Two","do"));
-//        words.add("Three");
-//        words.add("Four");
-//        words.add("Five");
-//        words.add("Six");
-//        words.add("Seven");
-//        words.add("Eight");
-//        words.add("Nine");
-//        words.add("Ten");
+        words.add(new Word("One","lutti"));
+        words.add(new Word("Two","otiiko"));
+        words.add(new Word("Three","tolookosu"));
+        words.add(new Word("Four","oyyisa"));
+        words.add(new Word("Five","otiiko"));
+        words.add(new Word("Six","temmokka"));
+        words.add(new Word("Seven","kenekaku"));
+        words.add(new Word("Eight","kawinta"));
+        words.add(new Word("Nine","wo'e"));
+        words.add(new Word("Ten","na'aacha"));
 
         WordAdapter itemsAdapter=new WordAdapter(this, words);
 //        ArrayAdapter<Word> itemsAdapter=new ArrayAdapter<>(this,R.layout.list_layout,words);
--- app/src/main/java/project/tronku/miwokapplication/WordAdapter.java
+++ app/src/main/java/project/tronku/miwokapplication/WordAdapter.java
@@ -1,14 +1,43 @@
 package project.tronku.miwokapplication;
 
+import android.app.Activity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class WordAdapter extends ArrayAdapter<Word> {
+    
+    public WordAdapter(Activity context, ArrayList<Word> words) {
+        // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
+        // the second argument is used when the ArrayAdapter is populating a single TextView.
+        // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
+        // going to use this second argument, so it can be any value. Here, we used 0.
+        super(context, 0, words);
+    }
+
 
     @Override
     public View getView(int position,  View convertView,  ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+//        return super.getView(position, convertView, parent);
+        View listItemView=convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_layout, parent, false);
+        }
+
+        Word currentWord = getItem(position);
+
+        TextView english= (TextView) listItemView.findViewById(R.id.english);
+        english.setText(currentWord.getDefaultTranslation());
+
+        TextView miwok= (TextView) listItemView.findViewById(R.id.miwok);
+        miwok.setText(currentWord.getMiwokTranslation());
+
+        return listItemView;
     }
 
 }
