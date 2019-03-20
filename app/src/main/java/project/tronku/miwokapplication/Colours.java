5e6068b0d95b5641f2283756a569f96845be2711
Dheeraj Kotwani <dheeraj.kotwani41@gmail.com>
2019-03-20 19:49:22 +0530

    Modify Colours activity

--- app/src/main/java/project/tronku/miwokapplication/Colours.java
+++ app/src/main/java/project/tronku/miwokapplication/Colours.java
@@ -14,20 +14,19 @@ public class Colours extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_colours);
 
-        ArrayList<String> words = new ArrayList<String>();
+        ArrayList<Word> words = new ArrayList<Word>();
 
-        words.add("Red");
-        words.add("Orange");
-        words.add("White");
-        words.add("Blue");
-        words.add("Pink");
-        words.add("Yellow");
-        words.add("Brown");
-        words.add("Violet");
-        words.add("Indigo");
-        words.add("Black");
+        words.add(new Word("Red","weṭeṭṭi",R.drawable.color_red));
+        words.add(new Word("Green","chokokki",R.drawable.color_green));
+        words.add(new Word("Brown","ṭakaakki",R.drawable.color_brown));
+        words.add(new Word("Gray","ṭopoppi",R.drawable.color_gray));
+        words.add(new Word("Black","kululli",R.drawable.color_black));
+        words.add(new Word("White","kelelli",R.drawable.color_white));
+        words.add(new Word("Dusty Yellow","ṭopiisә",R.drawable.color_dusty_yellow));
+        words.add(new Word("Mustard Yellow","chiwiiṭә",R.drawable.color_mustard_yellow));
 
-        ArrayAdapter<String> itemsAdapter=new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,words);
+
+        WordAdapter itemsAdapter=new WordAdapter(this,words,R.color.colors);
         ListView listview=(ListView) findViewById(R.id.colors);
         listview.setAdapter(itemsAdapter);
     }
